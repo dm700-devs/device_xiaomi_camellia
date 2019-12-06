5f476fbcdab498dbba44ffa2fbcf96cde3d2f845
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    camellia: Use 4GB dalvik-heap config
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Silence vendor logspam
 include $(LOCAL_PATH)/configs/props/vendor_logtags.mk
 
