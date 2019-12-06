1005553c9e8cfd3f4fc13d9c0c4e9f3a10599471
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    camellia: Use 4GB dalvik-heap config
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
