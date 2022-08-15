c93730a09c76d8c1af5c317893c1989cc7a69924
Matsvei Niaverau <matvej2002@gmail.com>
2022-08-15 17:36:58 +0300

    camellia: Import power HAL extension
    * Added fts_ts touchscreen support
    * From https://github.com/LineageOS/android_device_xiaomi_surya/blob/c28fc6e3ebbc8467858beac9b268c3ade9df1cad/power/power-mode.cpp
    * Adapted for our Mediatek power HAL
    
    Change-Id: Ic2439c357aa45cc35940b0a14525d0034351d3ca

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,9 @@ TARGET_BOARD_PLATFORM := mt6833
 BOARD_HAS_MTK_HARDWARE := true
 BOARD_VENDOR := xiaomi
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
