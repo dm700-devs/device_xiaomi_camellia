eda4b52eb245c21078b363dc2e88bb95a7ed8174
SamarV-121 <samarvispute121@pm.me>
2022-08-31 15:08:35 +0530

    camellia: Label and compile health AIDL

--- device.mk
+++ device.mk
@@ -140,9 +140,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.mt6833 \
+    android.hardware.health-service.mt6833-recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
