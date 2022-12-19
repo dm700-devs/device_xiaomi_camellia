7fcc7e70f03d2d4092bdf8f73fedca65c264703e
LuK1337 <priv.luk@gmail.com>
2022-12-19 17:33:27 +0000

    camellia: Add libcamera_metadata shim for stripping out system camera cap
    Change-Id: I8f7772fc9ec15a00c4b630866aa7fc162a52c1aa

--- device.mk
+++ device.mk
@@ -120,6 +120,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CameraGo
 
+PRODUCT_PACKAGES += \
+    libcamera_metadata_shim
+
 # CAS
 PRODUCT_PACKAGES += \
     android.hardware.cas@1.2-service-lazy
--- extract-files.sh
+++ extract-files.sh
@@ -48,7 +48,6 @@ while [ "${#}" -gt 0 ]; do
     esac
     shift
 done
-
 if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
@@ -83,6 +82,9 @@ function blob_fixup {
 	;&
     vendor/bin/hw/camerahalserver)
         "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+        ;;
+    vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
+        grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
         ;;
     esac
 }
