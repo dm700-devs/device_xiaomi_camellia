f13fc60b8dcf77a7b0952123c07d3910ee59b7ad
Sushrut1101 <guptasushrut@gmail.com>
2023-02-15 23:54:22 +0530

    everpal: Switch to AOSP AIDL Sensors Multihal
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -334,8 +334,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1.vendor \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    android.hardware.sensors-service.multihal \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
