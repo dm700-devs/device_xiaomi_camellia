444effbb0033cddae225d42909835f585080b46f
EreN <akbaseren4751@gmail.com>
2023-02-15 23:54:22 +0530

    camellia: Link mtk thermal service against VNDK 32 libutils
    Fixes: https://gist.githubusercontent.com/MrErenK/2355a2df7b04e9b3f6e4cf4752ef4518/raw/4d070b9dcf4b9f80ee6c4c412a457516feb3a0d8/thermals.txt
    
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -89,6 +89,9 @@ function blob_fixup {
     vendor/bin/hw/camerahalserver)
         "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
         ;;
+    vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
+        "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+        ;;
     esac
 }
 
