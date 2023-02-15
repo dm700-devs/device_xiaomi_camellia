463f272ec9d5fdde80fb8012f27170478f99bfc7
Jiyong Park <jiyong@google.com>
2023-02-15 23:54:22 +0530

    everpal: mtkpower: Remove ndk_platform backend. Use the ndk backend.
    The ndk_platform backend will soon be deprecated because the ndk backend
    can serve the same purpose. This is to eliminate the confusion about
    having two variants (ndk and ndk_platform) for the same ndk backend.
    
    Bug: 161456198
    Test: m
    Change-Id: Ice91de775c8ece0399eb4f30dd93d92cf2351db9
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -84,6 +84,9 @@ function blob_fixup {
     vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service)
         "$PATCHELF" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "$2"
         ;;
+    vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
+        "$PATCHELF" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "${2}"
+        ;;
     esac
 }
 
