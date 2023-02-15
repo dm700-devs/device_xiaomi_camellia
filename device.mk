bef329b83c20ea4c494bdddcb37ac3a1eca82433
Sushrut1101 <guptasushrut@gmail.com>
2023-02-15 23:54:22 +0530

    everpal: Build the Health HAL from Source
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -155,6 +155,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery
 
 # Media
