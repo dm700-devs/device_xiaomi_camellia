e0893bd549bc1846e74af2a8036ef7fd6e72b249
bengris32 <bengris32@protonmail.ch>
2023-02-15 23:54:22 +0530

    everpal: Patchelf C2 service to link against `libavservices_minijail`
    * The libavservices_minijail_vendor library was substituted for
      the vendor variant of libavservices_minijail in Android 13.
    
    * Patchelf the C2 service to link against libavservices_minijail
      instead of libavservices_minijail_vendor.
    
    Change-Id: Icb749362590407222c1a22687059516cd8a0b78d
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -191,8 +191,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.2.vendor \
-    libavservices_minijail.vendor \
-    libavservices_minijail_vendor
+    libavservices_minijail.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
