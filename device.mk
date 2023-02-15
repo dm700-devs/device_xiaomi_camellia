8e027f081420fd8d149bb5708466ed2738101d91
Sushrut1101 <guptasushrut@gmail.com>
2023-02-16 00:03:36 +0530

    everpal: Kang IMS Blobs from `light` T
    * from Redmi/light/light:13/TP1A.220624.014/V14.0.1.0.TLSCNXM:user/release-keys
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -190,6 +190,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     IFAAService
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-service \
