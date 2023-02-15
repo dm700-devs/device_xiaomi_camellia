2e40f50de8f6e03d248db4a8c8491f68ece562f0
Sushrut1101 <guptasushrut@gmail.com>
2023-02-15 23:54:22 +0530

    everpal: Build Light 2.0 interface from Source
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
     libkeystore-engine-wifi-hidl:64 \
     libkeystore-wifi-hidl
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0.vendor:64
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
