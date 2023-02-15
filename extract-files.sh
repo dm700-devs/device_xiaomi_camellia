93d353e0b46a481eb478a57a01d022c99128b6ce
LinkBoi00 <linkdevel@protonmail.com>
2023-02-15 23:54:22 +0530

    everpal: Link camerahalserver service against VNDK 32 libutils
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -92,6 +92,9 @@ function blob_fixup {
     vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
         "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
         ;;
+    vendor/bin/hw/camerahalserver)
+        "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+        ;;
     esac
 }
 
