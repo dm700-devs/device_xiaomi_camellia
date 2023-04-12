b0a66ac12c70db5c3fe5b75007de1691b553d3a1
SamarV-121 <samarvispute121@pm.me>
2023-04-12 10:56:33 +0000

    camellia: Link older libutils for PQ HAL
    * Abort message:
     'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already
     owned'

--- device.mk
+++ device.mk
@@ -443,6 +443,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy.camellia \
