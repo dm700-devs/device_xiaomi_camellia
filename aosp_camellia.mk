b5ce94b2e284171a38225dbc332b4dc29caea80b
rio004 <riyon336@gmail.com>
2023-05-06 23:20:40 +0000

    camellia: Welcome PixelOS

--- aosp_camellia.mk
+++ aosp_camellia.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from camellia device
 $(call inherit-product, device/xiaomi/camellia/device.mk)
@@ -20,7 +20,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := arrow_camellia
+PRODUCT_NAME := aosp_camellia
 PRODUCT_DEVICE := camellia
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_BRAND := Redmi
