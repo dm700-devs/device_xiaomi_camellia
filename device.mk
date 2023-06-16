07b65df7458ee4123e61169392c065ab1cc97637
rio004 <riyon336@gmail.com>
2023-06-16 01:13:28 +0000

    camellia: Build sensor AIDL
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -379,6 +379,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.hardware.sensors-service.multihal \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
