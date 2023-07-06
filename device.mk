b5da79ae65d96cef76b9ab35b0be57756f543e00
rio004 <riyon336@gmail.com>
2023-07-06 22:09:38 +0000

    camellia: Move recovery init to rootdir

--- device.mk
+++ device.mk
@@ -354,7 +354,8 @@ PRODUCT_PACKAGES += \
     ueventd.mt6833.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.mt6833:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.mt6833
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6833:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.mt6833 \
+    $(LOCAL_PATH)/rootdir/etc/init.recovery.mt6833.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6833.rc
 
 # RCS
 PRODUCT_PACKAGES += \
