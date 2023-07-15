a21887513d956ac63ac28983ca6c454fff5566a1
bengris32 <bengris32@protonmail.ch>
2023-07-15 14:27:26 +0000

    camellia: Build stub mtkpower service
    * It seems even after adding a dummy libmtkperf_client library,
      camera still refuses to work, it seems MediaTek have used a
      mix of that client library, and direct HIDL calls to the power
      implementation. Build a stub mtkpower service to get around
      this limitation.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -328,6 +328,7 @@ PRODUCT_PACKAGES += \
     libmtkperf_client_vendor
 
 PRODUCT_PACKAGES += \
+    vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     vendor.mediatek.hardware.mtkpower@1.2.vendor
 
 # Radio
