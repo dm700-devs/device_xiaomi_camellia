55bd81038d73ec55a20526a8693f27e7825548de
rio004 <riyon336@gmail.com>
2023-08-10 10:50:43 +0000

    camellia: Import AOSP fingerprint HIDL
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -110,6 +110,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.camellia
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
