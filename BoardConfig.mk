26c30eee106f5a9eb51b2f306c4ec51d052a57b2
rio004 <riyon336@gmail.com>
2023-08-10 09:55:20 +0000

    camellia: sepolicy: Address some camera and fingerprint denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,6 +156,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # VNDK
 BOARD_VNDK_VERSION := current
