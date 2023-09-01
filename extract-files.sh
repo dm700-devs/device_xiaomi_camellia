4c2493f17fe05f032337e1d26524d4684e51712a
bengris32 <bengris32@protonmail.ch>
2023-09-01 21:20:02 +0100

    camellia: Use enable instead of start for Neural Networks service
    * The MTK Neural Networks service is started via a property trigger.
      This HAL starts a lot earlier than other HALs since it is started
      with "start" instead of "enable", the difference between them being
      that "start" will immediately cause the service to try and start, while
      "enable" effectively removes the "disabled" attribute from the service
      (so in the case of a HAL, it will be started with all the other services
      in class hal instead of immediately).
    
    * This initially didn't have any side effects until I noticed the Neural Networks
      service constantly crashing in offline charger mode. To fix this, simply
      convert the "start" command to an "enable" to ensure that this service is not
      only started at the right time (in class hal), but also only started when
      needed (and not in offline charger mode).
    
    Change-Id: Iacf310fb2211117aaf028ad6bf0814a7d447cf53
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.sh
+++ extract-files.sh
@@ -74,6 +74,9 @@ function blob_fixup {
         vendor/etc/init/vendor.mediatek.hardware.mtkpower@1.0-service.rc)
             echo "$(cat ${2}) input" > "${2}"
             ;;
+        vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
+            sed -i 's/start/enable/' "$2"
+            ;;
 	vendor/lib*/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
