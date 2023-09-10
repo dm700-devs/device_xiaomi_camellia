2fc01e8dc3507363e1e6b74e2c3dce587e547d9d
bengris32 <bengris32@protonmail.ch>
2023-09-10 18:09:41 +0000

    camellia: Switch back to AOSP bootctrl
    * Due to legal reasons

--- device.mk
+++ device.mk
@@ -99,15 +99,9 @@ PRODUCT_PACKAGES += \
 
 # Boot Control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-service
-
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
-
-PRODUCT_PACKAGES += \
-    libmtk_bsg \
-    libmtk_bsg.recovery
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery
 
 # Camera
 PRODUCT_PACKAGES += \
