3c1e4bb93002a880f0a19ef7f93f5be870d0a2b1
rio004 <riyon336@gmail.com>
2023-09-29 20:30:26 +0100

    camellia: Configure Thermal stack
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -230,6 +230,11 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/mediatek
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
