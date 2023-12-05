e2cac7a21bc33d4dae65a8abe21c50d49a2ab38d
Vanya010209 <mcpe11640@gmail.com>
2023-12-05 11:11:12 -0500

    AndroidProducts-for-crdroidA14

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_camellia.mk
+    $(LOCAL_DIR)/lineage_camellia.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_camellia-user \
-    aosp_camellia-userdebug \
-    aosp_camellia-eng
+    lineage_camellia-user \
+    lineage_camellia-userdebug \
+    lineage_camellia-eng
