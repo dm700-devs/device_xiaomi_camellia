a11c77b9004ebab0f150ce939558bfcdfabf7f20
rio004 <riyon336@gmail.com>
2023-12-06 18:22:38 +0000

    Revert "camellia: Link sensor libraries against stock libsensorndkbridge"
    This reverts commit d7295f2222de2c11ef40b42756a7243175f2cfb4.

--- device.mk
+++ device.mk
@@ -329,7 +329,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.camellia-multihal \
-    android.frameworks.sensorservice@1.0.vendor
+    android.frameworks.sensorservice@1.0.vendor \
+    libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
--- extract-files.sh
+++ extract-files.sh
@@ -74,11 +74,6 @@ function blob_fixup {
 	vendor/lib*/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
-	vendor/bin/mnld | \
-	vendor/lib*/libaalservice.so | \
-	vendor/lib64/libcam.utils.sensorprovider.so)
-            "${PATCHELF}" --replace-needed "libsensorndkbridge.so" "libsensorndkbridge-hidl.so" "$2"
-            ;;
 	vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml)
             sed -i 's/1.1/1.2/' "$2"
             ;;
