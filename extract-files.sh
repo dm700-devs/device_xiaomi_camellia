ba94897f5ea8f72fd42f589e1230f0e0127e4d2c
bengris32 <bengris32@protonmail.ch>
2024-01-20 17:47:32 +0000

    camellia: Update IMS from plato V14.0.7.0.ULQMIXM
    Change-Id: I9ab402b7d2bdacba06caaabda18e70c9a8189ae4
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: rio004 <riyon336@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup {
     case "$1" in
+	system_ext/lib64/libsink.so)
+            "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
+            ;;
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
